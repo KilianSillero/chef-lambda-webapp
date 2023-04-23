1b40070f95d1ac7160df1db4a1ff391faec30892
EC2 Default User <ec2-user@ip-172-31-9-123.eu-west-1.compute.internal>
2023-04-23 16:49:52 +0000

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "1maa8lss4lp91tbfmpf6sdja1t",     // CognitoClientID
-  "api_base_url": "https://2pfhf4euzh.execute-api.eu-west-1.amazonaws.com/prod",                                     // RecipeFunctionApi
+  "aws_user_pools_web_client_id": "27t5sff4nkdherp6bkge20f9e2",     // CognitoClientID
+  "api_base_url": "https://yzms6pewqj.execute-api.eu-west-1.amazonaws.com/prod",                                     // RecipeFunctionApi
   "cognito_hosted_domain": "myrecipeappdemo-chef-lambda-webapp.auth.eu-west-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d1e68y1rm33pqb.amplifyapp.com"                                      // AmplifyURL
+  "redirect_url": "https://master.d1eak4r8g1jfjf.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
